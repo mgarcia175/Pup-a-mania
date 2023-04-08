103447c38371fc5f0584665e25c0e5a26683d48f
Martin <martingarcia804@yahoo.com>
2023-04-07 23:05:40 -0500

    Made dropdown work on click and appended breeds to dropdown list

--- index.js
+++ index.js
@@ -19,17 +19,48 @@ function materializeRandomDogImg(data) {
     dogImg.src = data
     dogImg.id = "dog-image"
 
-    console.log(data)
-
     Ul.append(dogImg)
 }
 }
 
+//Loops for every breed name and capitalizes first letter//
+function fetchBreedNames() {
+fetch('https://dog.ceo/api/breeds/list/all')
+.then(res => res.json())
+.then(data => materializeBreedNames(data.message))
+}
+
+function materializeBreedNames(data) {
+
+    for(let breed in data) {
+      let dropDown = document.getElementById("breedListDropdown")
+      const breedName = (breed[0].toUpperCase() + breed.substring(1))
+      let breedCount = document.createElement("a")
+      breedCount.href = "#"
+      breedCount.innerHTML = breedName
+
+      dropDown.append(breedCount)
 
 
+    }
+}
 
+fetchBreedNames()
+//Loops for every breed name and capitalizes first letter//
 
+//Function for dropdown
+let dropdwnButton = document.getElementById("dropbtn")
+let breedList = document.getElementById("breedListDropdown")
+
+dropdwnButton.addEventListener("click", function() {
+  if(breedList.className === "breed-dropdown-content") {
+    breedList.className = "show"
+  } else if (breedList.className === "show") {
+    breedList.className = "breed-dropdown-content"
+  }
+
+})
+//Function for dropdown
 
 
 })
-
